8e8f23cd7500fc7047bc1ad2f5f2492219a24ce1
dimloldev <wot.mits@gmail.com>
2025-01-06 16:30:52 +0200

    Add Chrome service and options imports to server.py for enhanced WebDriver configuration

--- api/server.py
+++ api/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
 
